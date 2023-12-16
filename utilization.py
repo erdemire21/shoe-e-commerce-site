b840b59e0b4fc7ffe446c9bab2b55f1033d59127
Emre Hakan Erdemir <92296896+erdemire21@users.noreply.github.com>
2023-12-16 19:28:33 +0000

    utilization to use

---
+++ utilization.py
@@ -0,0 +1,119 @@
+import mysql.connector
+from mysql.connector import Error
+
+db_config = {
+    'host': 'localhost',
+    'database': 'login',
+    'user': 'root',
+    'password': 'E19741453'
+}
+
+def add_user_to_database(username, email, password):
+    """
+    Add a new user to the database.
+
+    Args:
+        username (str): The username of the new user.
+        email (str): The email of the new user.
+        password (str): The password of the new user.
+
+    Returns:
+        str: A message indicating the result of the registration process.
+
+    Raises:
+        Error: If there is an error during the database operation.
+    """
+    try:
+        connection = mysql.connector.connect(**db_config)
+        cursor = connection.cursor()
+
+        # Check if the email is already in use
+        check_query = "SELECT * FROM email_pswd WHERE email = %s"
+        cursor.execute(check_query, (email,))
+        existing_user = cursor.fetchone()
+
+        if existing_user:
+            return "Email is already in use. Choose a different one."
+
+        # Insert the new user into the database
+        insert_query = "INSERT INTO email_pswd (create_time, name, email, password) VALUES (NOW(), %s, %s, %s)"
+        cursor.execute(insert_query, (username, email, password))
+        connection.commit()
+
+        return "Registration successful!"
+
+    except Error as e:
+        return f"Error: {e}"
+
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+
+
+def authenticate_user(email, password):
+    """
+    Authenticates a user by checking their email and password against the database.
+
+    Args:
+        email (str): The email of the user.
+        password (str): The password of the user.
+
+    Returns:
+        bool: True if the user is authenticated, False otherwise.
+    """
+    try:
+        connection = mysql.connector.connect(**db_config)
+        cursor = connection.cursor()
+
+        # Check user credentials
+        query = "SELECT * FROM email_pswd WHERE email = %s AND password = %s"
+        cursor.execute(query, (email, password))
+        user = cursor.fetchone()
+
+        if user:
+            return True
+        else:
+            return False
+
+    except Error as e:
+        print(f"Error: {e}")
+        return False
+
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+
+def is_valid_credit_card(card_number):
+    """
+    Check if a credit card number is valid.
+
+    Args:
+        card_number (str): The credit card number to be validated.
+
+    Returns:
+        bool: True if the credit card number is valid, False otherwise.
+    """
+    # Remove spaces and reverse the card number
+    card_number = card_number.replace(" ", "")[::-1]
+
+    # Ensure the card number is a valid numeric string
+    if not card_number.isdigit():
+        return False
+
+    total = 0
+    alternate = False
+
+    for digit in card_number:
+        digit = int(digit)
+
+        if alternate:
+            digit *= 2
+            if digit > 9:
+                digit -= 9
+
+        total += digit
+        alternate = not alternate
+
+    return total % 10 == 0
