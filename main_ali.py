d8fab07854b07c74f923ed6a39319aed73e0ee53
Alihan Yalçın <147749511+yalcinalihan@users.noreply.github.com>
2023-12-18 20:12:31 +0300

    main and util py files for my part

---
+++ main_ali.py
@@ -0,0 +1,81 @@
+from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify
+import mysql.connector
+from mysql.connector import Error
+from utilization_ali import *
+
+app = Flask(__name__)
+app.secret_key = '111'
+
+
+db_config ={
+    'host': 'sql11.freemysqlhosting.net',
+    'database': 'sql11670983',
+    'user': 'sql11670983',
+    'password': 'T7p8T65KtN',
+}
+connection = mysql.connector.connect(**db_config)
+cursor = connection.cursor()
+
+create_table_query = '''
+    CREATE TABLE IF NOT EXISTS products (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        price DECIMAL(10, 2) NOT NULL
+    )
+'''
+cursor.execute(create_table_query)
+
+email = "'t@t.com'"
+userId = 1
+product = get_all_shoes()
+
+order = get_orders(email)
+
+@app.route('/')
+def browse_items():
+    return render_template('listing.html', products=product)
+
+
+
+@app.route('/user_page')
+def user_page():
+    return render_template('user_page.html', orders=order)
+
+
+@app.route('/add_item/<string:item_id>')
+def add_item(item_id):
+    connection = mysql.connector.connect(**db_config)
+    cursor = connection.cursor()    
+    add_query= "INSERT INTO deneme (Model) VALUES (%s);"
+    cursor.execute(add_query, (item_id,))
+
+    connection.commit()
+
+    cursor.close()
+    connection.close()
+    return render_template('listing.html', products=product)
+
+    
+@app.route('/add_to_database', methods=['POST'])
+def add_to_database():
+    data = request.json
+    cart = data.get('cart', [])
+
+    try:
+        # Insert items from the cart into the MySQL table
+        insert_query = 'INSERT INTO products (name, price) VALUES (%s, %s)'
+        for product in cart:
+            cursor.execute(insert_query, (product['name'], product['price']))
+        # Commit the changes to the database
+        connection.commit()
+
+        return jsonify(result='success')
+
+    except Exception as e:
+        print(f"Error inserting into database: {e}")
+        connection.rollback()
+        return jsonify(result='error')
+
+# Your existing routes and other code...
+
+app.run(debug=True)
---
+++ utilization_ali.py
@@ -0,0 +1,45 @@
+import mysql.connector
+from mysql.connector import Error
+
+db_config ={
+    'host': 'sql11.freemysqlhosting.net',
+    'database': 'sql11670983',
+    'user': 'sql11670983',
+    'password': 'T7p8T65KtN',
+}
+
+
+def get_all_shoes():
+    # Use a context manager for the cursor
+    try:
+        connection = mysql.connector.connect(**db_config)
+
+        with connection.cursor() as cursor:
+            display_all = "SELECT Brand, Model, Type, Gender, Size, Color, Material, Price, Image_URL FROM shoe_info;"
+            cursor.execute(display_all)
+            shoes = cursor.fetchall()
+
+        # The connection will be closed automatically when exiting the 'with' block
+
+        return shoes
+
+    except mysql.connector.Error as err:
+        # Handle database errors
+        print(f"Error: {err}")
+        return None
+
+def get_orders(email):
+    connection = mysql.connector.connect(**db_config)
+    cursor = connection.cursor()
+
+    u_orders = "SELECT * FROM  orders WHERE orders.email="+email+";"
+    cursor.execute(u_orders)
+    orders = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+
+    return orders
+
+
+
