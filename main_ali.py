a5dac207f8dbcef075dd295e5f732c0d9acf9da7
Alihan Yalçın <147749511+yalcinalihan@users.noreply.github.com>
2023-12-19 12:27:42 +0300

    add_Cart duzeltildi

--- main_ali.py
+++ main_ali.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify
 import mysql.connector
 from mysql.connector import Error
-from utilization_ali import *
+from util_ismail import *
 
 app = Flask(__name__)
 app.secret_key = '111'
@@ -16,15 +16,6 @@ db_config ={
 connection = mysql.connector.connect(**db_config)
 cursor = connection.cursor()
 
-create_table_query = '''
-    CREATE TABLE IF NOT EXISTS products (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(255) NOT NULL,
-        price DECIMAL(10, 2) NOT NULL
-    )
-'''
-cursor.execute(create_table_query)
-
 email = "'t@t.com'"
 userId = 1
 product = get_all_shoes()
@@ -33,7 +24,8 @@ order = get_orders(email)
 
 @app.route('/')
 def browse_items():
-    return render_template('listing.html', products=product)
+    return render_template('deneme.html', products=product)
+
 
 
 
@@ -42,17 +34,13 @@ def user_page():
     return render_template('user_page.html', orders=order)
 
 
-from flask import Flask, render_template
-import mysql.connector
-
-app = Flask(_name_)
-
 # Assuming you have db_config defined somewhere with your MySQL database configuration
 
 @app.route('/add_item', methods=['POST'])
 def add_item():
     item_id = request.form.get('item_id')
     price = request.form.get('price')
+    price = float(str(price[1:]))
     quantity = request.form.get('quantity')
     email = request.form.get('email')
     brand = request.form.get('brand')
@@ -62,11 +50,13 @@ def add_item():
     cursor = connection.cursor()
 
     # Check if the item with the given item_id already exists
-    select_query = "SELECT * FROM shopping_cart WHERE item_id = %s;"
+    
+    select_query = "SELECT quantity FROM shopping_cart WHERE item_id = %s"
     cursor.execute(select_query, (item_id,))
-    existing_item = cursor.fetchone()
+    quan = cursor.fetchone()
+    
 
-    if existing_item:
+    if quan:
         # If the item exists, update the quantity
         update_query = "UPDATE shopping_cart SET Quantity = Quantity + 1 WHERE item_id = %s;"
         cursor.execute(update_query, (item_id,))
@@ -79,29 +69,7 @@ def add_item():
 
     cursor.close()
     connection.close()
-    return render_template('listing.html', products=product)  
+    return render_template('deneme.html', products=product)  
 
     
-@app.route('/add_to_database', methods=['POST'])
-def add_to_database():
-    data = request.json
-    cart = data.get('cart', [])
-
-    try:
-        # Insert items from the cart into the MySQL table
-        insert_query = 'INSERT INTO products (name, price) VALUES (%s, %s)'
-        for product in cart:
-            cursor.execute(insert_query, (product['name'], product['price']))
-        # Commit the changes to the database
-        connection.commit()
-
-        return jsonify(result='success')
-
-    except Exception as e:
-        print(f"Error inserting into database: {e}")
-        connection.rollback()
-        return jsonify(result='error')
-
-# Your existing routes and other code...
-
 app.run(debug=True)
